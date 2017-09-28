91041fe661130a51d56e4c0005343088669e0491
Anurag Kumar <anurag105031@outlook.com>
2017-09-28 19:59:14 +0530

    Updated reg test

--- src/test/java/com/selenium/automationpractice/registration/TC_02_registration.java
+++ src/test/java/com/selenium/automationpractice/registration/TC_02_registration.java
@@ -2,6 +2,7 @@ package com.selenium.automationpractice.registration;
 
 import java.io.IOException;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,15 +21,17 @@ public class TC_02_registration extends Testbase {
 	}
 	
 	@Test
-	public void verifyLoginWithInvalidCredentails() throws InterruptedException{
+	public void verifyLoginWithInvalidCredentails() throws InterruptedException, IOException{
+		regPage = new RegistrationPage(driver);
 		regPage.registration();
+		regPage.getScreenshot("image1");
 		
 	}
 	
-//	@AfterClass
-//	public void endTest(){
-//		driver.close();
-//	}
+	@AfterMethod
+	public void endTest(){
+		driver.close();
+	}
 	
 	
 }
